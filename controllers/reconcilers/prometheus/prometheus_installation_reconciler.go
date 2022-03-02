99763f593a1f30f558776ee70ebfd3654481e751
rohan47 <rohanrgupta1996@gmail.com>
2022-03-02 23:18:48 +0530

    Added code to start prometheus instance
    added code to start prometheus instance
    
    Signed-off-by: Rohan Gupta <rohgupta@redhat.com>

---
+++ controllers/reconcilers/prometheus/prometheus_installation_reconciler.go
@@ -0,0 +1,300 @@
+package prometheus
+
+import (
+	"context"
+	"reflect"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	//ctrl "sigs.k8s.io/controller-runtime"
+	corev1alpha1 "github.com/operator-framework/api/pkg/operators/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	//"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	"github.com/RHEcosystemAppEng/dbaas-operator/api/v1alpha1"
+	"github.com/RHEcosystemAppEng/dbaas-operator/controllers/reconcilers"
+	"github.com/go-logr/logr"
+	operatorsv1 "github.com/operator-framework/api/pkg/operators/v1"
+	promv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+)
+
+const (
+	Namespace         = "openshift-dbaas-prometheus"
+	subscriptionName  = "dbaas-operator-prometheus-operator"
+	operatorGroupName = "dbaas-operator-prometheus-operator"
+	prometheusName    = "dbaas-operator-prometheus"
+	prometheusCSV     = "prometheusoperator.0.47.0"
+
+	managedBy    = "app.kubernetes.io/managed-by"
+	operatorName = "dbaas-operator"
+)
+
+type Reconciler struct {
+	client     client.Client
+	prometheus *promv1.Prometheus
+	cr         *v1alpha1.DBaaSPlatform
+	scheme     *runtime.Scheme
+	log        logr.Logger
+}
+
+func NewReconciler(cr *v1alpha1.DBaaSPlatform, client client.Client, scheme *runtime.Scheme, log logr.Logger) reconcilers.PlatformReconciler {
+	return &Reconciler{
+		client:     client,
+		prometheus: &promv1.Prometheus{},
+		cr:         cr,
+		scheme:     scheme,
+		log:        log,
+	}
+}
+func (r *Reconciler) Reconcile(ctx context.Context, cr *v1alpha1.DBaaSPlatform, status2 *v1alpha1.DBaaSPlatformStatus) (v1alpha1.PlatformsInstlnStatus, error) {
+
+	status, err := r.reconcileNamespace(ctx)
+	if status != v1alpha1.ResultSuccess {
+		return status, err
+	}
+
+	status, err = r.reconcileOperatorGroup(ctx)
+	if status != v1alpha1.ResultSuccess {
+		return status, err
+	}
+
+	status, err = r.reconcileSubscription(ctx)
+	if status != v1alpha1.ResultSuccess {
+		return status, err
+	}
+
+	//status, err = r.reconcileDeployment(ctx)
+	//if status != v1alpha1.ResultSuccess {
+	//	return status, err
+	//}
+
+	return v1alpha1.ResultSuccess, nil
+}
+
+func (r *Reconciler) Cleanup(ctx context.Context, cr *v1alpha1.DBaaSPlatform) (v1alpha1.PlatformsInstlnStatus, error) {
+
+	deployment := r.prometheus
+	err := r.client.Delete(ctx, deployment)
+	if err != nil && !errors.IsNotFound(err) {
+		return v1alpha1.ResultFailed, err
+	}
+
+	return v1alpha1.ResultSuccess, nil
+}
+
+func (r *Reconciler) reconcileNamespace(ctx context.Context) (v1alpha1.PlatformsInstlnStatus, error) {
+
+	key := types.NamespacedName{Name: Namespace}
+	var namespace corev1.Namespace
+	err := r.client.Get(ctx, key, &namespace)
+	if err != nil && !errors.IsNotFound(err) {
+		return v1alpha1.ResultFailed, err
+	}
+
+	if errors.IsNotFound(err) {
+		r.log.Info("Creating namespace")
+		err = r.client.Create(ctx, NewNamespace())
+		return v1alpha1.ResultFailed, err
+	}
+
+	// requeue if namespace is marked for deletion
+	// TODO(sthaha): decide if want to use finalizers to prevent deletion but
+	// we also need to solve how to properly cleanup / uninstall operator
+	if namespace.Status.Phase != corev1.NamespaceActive {
+		r.log.Info("Namespace is present but not active", "phase", namespace.Status.Phase)
+		return v1alpha1.ResultInProgress, nil
+	}
+	return v1alpha1.ResultSuccess, nil
+}
+
+func (r *Reconciler) reconcileOperatorGroup(ctx context.Context) (v1alpha1.PlatformsInstlnStatus, error) {
+	log := r.log.WithValues("Name", operatorGroupName)
+	log.V(6).Info("Reconciling OperatorGroup")
+
+	key := types.NamespacedName{
+		Name:      operatorGroupName,
+		Namespace: Namespace,
+	}
+	var operatorGroup operatorsv1.OperatorGroup
+
+	err := r.client.Get(ctx, key, &operatorGroup)
+	if err != nil && !errors.IsNotFound(err) {
+		return v1alpha1.ResultFailed, err
+	}
+
+	// create
+	desired := newOperatorGroup()
+	if errors.IsNotFound(err) {
+		log.Info("Creating OperatorGroup")
+		err := r.client.Create(ctx, desired)
+		return v1alpha1.ResultFailed, err
+	}
+
+	// update
+	if !reflect.DeepEqual(operatorGroup.Spec, desired.Spec) {
+		log.Info("Updating OperatorGroup")
+		operatorGroup.Spec = desired.Spec
+		err := r.client.Update(ctx, &operatorGroup)
+		if err != nil {
+			return v1alpha1.ResultFailed, err
+		}
+	}
+
+	return v1alpha1.ResultSuccess, nil
+}
+
+func (r *Reconciler) reconcileSubscription(ctx context.Context) (v1alpha1.PlatformsInstlnStatus, error) {
+	log := r.log.WithValues("Name", subscriptionName)
+	key := types.NamespacedName{
+		Name:      subscriptionName,
+		Namespace: Namespace,
+	}
+	var subscription corev1alpha1.Subscription
+	err := r.client.Get(ctx, key, &subscription)
+	if err != nil && !errors.IsNotFound(err) {
+		return v1alpha1.ResultFailed, err
+	}
+
+	// create
+	desired := NewSubscription()
+	if errors.IsNotFound(err) {
+		log.Info("Creating Grafana Operator Subscription")
+		err := r.client.Create(ctx, desired)
+		if err != nil {
+			return v1alpha1.ResultFailed, err
+		}
+		return v1alpha1.ResultInProgress, nil
+	}
+
+	if subscription.Spec.StartingCSV == desired.Spec.StartingCSV {
+		return v1alpha1.ResultSuccess, nil
+	}
+
+	r.log.WithValues("Name", subscription.Name).Info("Deleting Subscription")
+	if err := r.client.Delete(ctx, &subscription); err != nil {
+		return v1alpha1.ResultFailed, err
+	}
+
+	r.log.WithValues("Name", subscription.Status.InstalledCSV).Info("Deleting CSV")
+	csv := corev1alpha1.ClusterServiceVersion{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: corev1alpha1.SchemeGroupVersion.String(),
+			Kind:       "ClusterServiceVersion",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      subscription.Status.InstalledCSV,
+			Namespace: Namespace,
+			Labels:    commonLabels(),
+		},
+	}
+	if err := r.client.Delete(ctx, &csv); err != nil {
+		return v1alpha1.ResultFailed, err
+	}
+
+	r.log.WithValues("Name", subscription.Name).Info("Creating Subscription")
+	err = r.client.Create(ctx, &subscription)
+	if err != nil {
+		return v1alpha1.ResultFailed, err
+	}
+	return v1alpha1.ResultSuccess, nil
+}
+
+func NewSubscription() *corev1alpha1.Subscription {
+	return &corev1alpha1.Subscription{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: corev1alpha1.SchemeGroupVersion.String(),
+			Kind:       "Subscription",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      subscriptionName,
+			Namespace: Namespace,
+			Labels:    commonLabels(),
+		},
+		Spec: &corev1alpha1.SubscriptionSpec{
+			CatalogSource:          "",
+			CatalogSourceNamespace: "",
+			Package:                "dbass-operator",
+			Channel:                "beta",
+			InstallPlanApproval:    corev1alpha1.ApprovalManual,
+			StartingCSV:            prometheusCSV,
+			Config: &corev1alpha1.SubscriptionConfig{
+				Env: []corev1.EnvVar{
+					{
+						Name:  "DASHBOARD_NAMESPACES_ALL",
+						Value: "true",
+					},
+				},
+			},
+		},
+	}
+}
+
+// func (r *Reconciler) reconcileDeployment(ctx context.Context) (v1alpha1.PlatformsInstlnStatus, error) {
+// 	_, err := controllerutil.CreateOrUpdate(ctx, r.client, r.prometheus, func() error {
+// 		// if err := r.own(r.prometheus); err != nil {
+// 		// 	return err
+// 		// }
+
+// 		desired := PrometheusTemplate.DeepCopy()
+// 		r.prometheus.Spec = desired.Spec
+// 		return nil
+// 	})
+
+// 	if err != nil {
+// 		if errors.IsConflict(err) {
+// 			return v1alpha1.ResultInProgress, nil
+// 		}
+// 		return v1alpha1.ResultFailed, err
+// 	}
+// 	return v1alpha1.ResultSuccess, nil
+// }
+
+// func (r *Reconciler) own(resource metav1.Object) error {
+//	 if err := ctrl.SetControllerReference(r.cr, resource, r.scheme); err != nil {
+//		 return err
+//	 }
+//	 return nil
+//  }
+
+func NewNamespace() *corev1.Namespace {
+	return &corev1.Namespace{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: corev1.SchemeGroupVersion.String(),
+			Kind:       "Namespace",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   Namespace,
+			Labels: commonLabels(),
+		},
+	}
+}
+
+func commonLabels() map[string]string {
+	return map[string]string{
+		managedBy: operatorName,
+	}
+}
+
+func newOperatorGroup() *operatorsv1.OperatorGroup {
+	return &operatorsv1.OperatorGroup{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: operatorsv1.SchemeGroupVersion.String(),
+			Kind:       "OperatorGroup",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      operatorGroupName,
+			Namespace: Namespace,
+			Labels:    commonLabels(),
+		},
+		Spec: operatorsv1.OperatorGroupSpec{
+			TargetNamespaces: []string{
+				Namespace,
+			},
+		},
+	}
+}
